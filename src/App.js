e7f9a79f4798341e12c8dff91917fcac2fd39e0c
Rumman Bin Ashraf <rumman.ashraf@gmail.com>
2020-02-22 09:02:06 +0600

    Added working with dynamic Components example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi I am a React App Enthusiast</h1>
+        <p>This is really working!</p>
+        <Person name="Rumman" age="29"/>
+        <Person name="Abir" age="20"/>
+        <Person name="Mahe" age="21"/>
       </div>
     );
 
