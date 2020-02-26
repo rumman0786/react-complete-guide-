0aa7a259e3df5216c8b440a08df081422f7c3fee
Rumman Bin Ashraf <rumman@therapservices.net>
2020-02-26 10:53:24 +0600

    Added special state variable usage and event handler code
    Upto lesson 40

--- src/App.js
+++ src/App.js
@@ -3,14 +3,27 @@ import './App.css';
 import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons: [
+      {name: 'Rumman', age:29},
+      {name: 'Abir', age:20},
+      {name: 'Mahe', age:22}
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was clicked!");
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi I am a React App Enthusiast</h1>
         <p>This is really working!</p>
-        <Person name="Rumman" age="29"/>
-        <Person name="Abir" age="20"/>
-        <Person name="Mahe" age="21"/>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Another Hobby of Gardenning</Person>
       </div>
     );
 
