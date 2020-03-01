8bc7a99f3f6ea7af73e959f58ad4699482d97b6f
Rumman Bin Ashraf <rumman@therapservices.net>
2020-03-01 09:34:33 +0600

    Added useState hook example

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person'
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personState, setPersonState] = useState({
     persons: [
       {name: 'Rumman', age:29},
       {name: 'Abir', age:20},
       {name: 'Mahe', age:22}
     ]
+  });
+
+  const [otherState, setOtherState] = useState("Some Other value");
+  
+  console.log(personState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonState({
+      persons: [
+        {name: 'Imran', age:35},
+        {name: 'Abir', age:20},
+        {name: 'Mahe', age:27}
+      ]  
+    });
   }
 
-  switchNameHandler = () => {
-    console.log("Was clicked!");
-  }
+  return (
+    <div className="App">
+      <h1>Hi I am a React App Enthusiast</h1>
+      <p>This is really working!</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personState.persons[0].name} age={personState.persons[0].age}/>
+      <Person name={personState.persons[1].name} age={personState.persons[1].age}/>
+      <Person name={personState.persons[2].name} age={personState.persons[2].age}>Another Hobby of Gardenning</Person>
+      <div>{otherState}</div>
+    </div>
+  );
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi I am a React App Enthusiast</h1>
-        <p>This is really working!</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Another Hobby of Gardenning</Person>
-      </div>
-    );
-
-    // Above compiled code will look like below
-    // return React.createElement('div', {className: 'App'},
-    //   React.createElement('h1', null, 'Hi! This is the content of the h1 tag')
-    // );
-  }
+  // Above compiled code will look like below
+  // return React.createElement('div', {className: 'App'},
+  //   React.createElement('h1', null, 'Hi! This is the content of the h1 tag')
+  // );
 }
 
 export default App;
